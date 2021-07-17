749d81657315a7418562a8c3620b3c3d87af758a
James <rfsjim@users.noreply.github.com>
2021-07-18 09:02:40 +1000

    Bug with delviering energy to terminal, need to put a cap on energy.

--- role.longDistanceHarvester.js
+++ role.longDistanceHarvester.js
@@ -24,8 +24,7 @@ module.exports = {
                     // we use the arrow operator to define it
                     filter: (s) => (s.structureType == STRUCTURE_SPAWN
                                  || s.structureType == STRUCTURE_EXTENSION
-                                 || s.structureType == STRUCTURE_TOWER
-                                 || s.structureType == STRUCTURE_TERMINAL)
+                                 || s.structureType == STRUCTURE_TOWER)
                                  && s.store[RESOURCE_ENERGY] < s.store.getCapacity(RESOURCE_ENERGY)
                 });
 
